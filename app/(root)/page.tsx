8eda7e96b3c4b3d313fe6eaf585ab955b13684d1
robertWalker68501 <robert.walker6580@gmail.com>
2025-08-05 02:14:41 -0700

    setup auth layouts, social auth form, next auth, oauth with google and github providers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-const Home = () => {
+import { auth, signOut } from '@/auth';
+import { Button } from '@/components/ui/button';
+import ROUTES from '@/constants/routes';
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <div>
       <h1 className='text-2xl'>Home Page</h1>
+
+      <form
+        className='px-10 pt-[100px]'
+        action={async () => {
+          'use server';
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type='submit'>Logout</Button>
+      </form>
     </div>
   );
 };
---
+++ components/forms/SocialAuthForm.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import Image from 'next/image';
+import { signIn } from 'next-auth/react';
+import React from 'react';
+import ROUTES from '@/constants/routes';
+import { toast } from 'sonner';
+import { Button } from '../ui/button';
+
+const SocialAuthForm = () => {
+  const buttonClass =
+    'background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5';
+
+  const handleSignIn = async (provider: 'github' | 'google') => {
+    try {
+      await signIn(provider, {
+        redirectTo: ROUTES.HOME,
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast('An error occurred while signing in', {
+        classNames: {
+          toast: 'toast-error',
+        },
+      });
+    }
+  };
+
+  return (
+    <div className='mt-10 flex flex-wrap gap-2.5'>
+      <Button
+        className={buttonClass}
+        onClick={() => handleSignIn('github')}
+      >
+        <Image
+          src='/icons/github.svg'
+          alt='Github Logo'
+          width={20}
+          height={20}
+          className='invert-colors mr-2.5 object-contain'
+        />
+        <span>Log in with GitHub</span>
+      </Button>
+
+      <Button
+        className={buttonClass}
+        onClick={() => handleSignIn('google')}
+      >
+        <Image
+          src='/icons/google.svg'
+          alt='Google Logo'
+          width={20}
+          height={20}
+          className='mr-2.5 object-contain'
+        />
+        <span>Log in with Google</span>
+      </Button>
+    </div>
+  );
+};
+
+// @ts-expect-error not sure
+export default SocialAuthForm;
