40eb6e90c375e6c96c258f3bcb4abf0a5bf0aac4
robertWalker68501 <robert.walker6580@gmail.com>
2025-08-05 17:21:17 -0700

    create home page layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,31 @@
-import { auth } from '@/auth';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import ROUTES from '@/constants/routes';
+import LocalSearch from '@/components/search/LocalSearch';
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
-
   return (
-    <div>
-      <h1 className='text-2xl'>Home Page</h1>
-    </div>
+    <>
+      <section className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+        <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+        <Button
+          className='primary-gradient !text-light-900 min-h-[46px] px-4 py-3'
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className='mt-11'>
+        <LocalSearch />
+      </section>
+      HomeFilter
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
+    </>
   );
 };
 
---
+++ components/search/LocalSearch.tsx
@@ -0,0 +1,9 @@
+const LocalSearch = () => {
+  return (
+    <div>
+      <h1>LocalSearch</h1>
+    </div>
+  );
+};
+
+export default LocalSearch;
