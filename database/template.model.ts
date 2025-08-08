e9f16318b06dbfe003b167c5bbdd7fb1b218d5aa
robertWalker68501 <robert.walker6580@gmail.com>
2025-08-07 23:49:42 -0700

    create vote model

--- database/template.model.ts
+++ database/template.model.ts
@@ -4,6 +4,6 @@ export interface IModel {}
 
 const ModelSchema = new Schema<IModel>({}, { timestamps: true });
 
-const Model = models?.Model || model<IModel>('IModel', ModelSchema);
+const Model = models?.Model || model<IModel>('Model', ModelSchema);
 
 export default Model;
