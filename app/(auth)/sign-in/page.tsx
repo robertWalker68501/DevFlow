aae2a891711eb0b5d0ccca5c703759711fe99938
robertWalker68501 <robert.walker6580@gmail.com>
2025-08-14 13:09:17 -0700

    implement sign in with email and password

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,6 +2,7 @@
 
 import AuthForm from '@/components/forms/AuthForm';
 import { SignInSchema } from '@/lib/validations';
+import { signInWithCredentials } from '@/lib/actions/auth.action';
 
 const SignInPage = () => {
   return (
@@ -12,7 +13,7 @@ const SignInPage = () => {
         email: '',
         password: '',
       }}
-      onSubmit={(data) => Promise.resolve({ success: true, data })}
+      onSubmit={signInWithCredentials}
     />
   );
 };
